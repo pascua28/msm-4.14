ccaa200699f43f0b0565248c0fcf84a8a653d751
Samuel Pascua <sgpascua@ngcp.ph>
2021-04-26 11:05:09 +0800

    security: selinux: cleanup oppo changes
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -760,15 +760,7 @@ out:
 	kfree(n);
 	kfree(t);
 
-#ifdef VENDOR_EDIT
-/* Xianlin.Wu@ROM.Security, 2019/07/27, add for disallow toggling the kernel
- * between enforcing mode and permissive mode via /selinux/enforce or
- * selinux_enforcing symbol in normal/silence mode of release build.
- */
-    if (!is_selinux_enforcing())
-#else
 	if (!selinux_enforcing)
-#endif /* VENDOR_EDIT */
 		return 0;
 	return -EPERM;
 }
@@ -1551,15 +1543,7 @@ out:
 	kfree(s);
 	kfree(t);
 	kfree(n);
-#ifdef VENDOR_EDIT
-/* Xianlin.Wu@ROM.Security, 2019/07/27, add for disallow toggling the kernel
- * between enforcing mode and permissive mode via /selinux/enforce or
- * selinux_enforcing symbol in normal/silence mode of release build.
- */
-    if (!is_selinux_enforcing())
-#else
 	if (!selinux_enforcing)
-#endif /* VENDOR_EDIT */
 		return 0;
 	return -EACCES;
 }
@@ -1850,15 +1834,7 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	char *s;
 	u32 len;
 
-#ifdef VENDOR_EDIT
-/* Xianlin.Wu@ROM.Security, 2019/07/27, add for disallow toggling the kernel
- * between enforcing mode and permissive mode via /selinux/enforce or
- * selinux_enforcing symbol in normal/silence mode of release build.
- */
-    if (is_selinux_enforcing())
-#else
 	if (selinux_enforcing)
-#endif /* VENDOR_EDIT */
 		return -EINVAL;
 
 	if (!context_struct_to_string(context, &s, &len)) {
