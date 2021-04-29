9560c39b0d09090b9024ac7a0ff9cb0216779ded
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-04-29 20:13:24 +0800

    sched: remove per-tick cpu task accounting
    This is an expensive operation with only the results used for debugging/by userspace
    
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1525,18 +1525,6 @@ static struct ctl_table kern_table[] = {
         .proc_handler = proc_dointvec,
     },
 #endif /* VENDOR_EDIT */
-#ifdef VENDOR_EDIT
-/* Hailong.Liu@TECH.Kernel.CPU, 2019/10/24, stat cpu usage on each tick. */
-	{
-		.procname	= "task_cpustats_enable",
-		.data		= &sysctl_task_cpustats_enable,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0666,
-		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= &zero,
-		.extra2		= &one,
-	},
-#endif
 	{ }
 };
 
