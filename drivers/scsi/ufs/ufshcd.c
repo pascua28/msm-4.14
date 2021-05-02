611781fa45b645965747991a16fe604190a93ce6
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-05-02 18:54:18 +0800

    scsi: ufs: add missed changes on pm_qos removal
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -3878,7 +3878,6 @@ static int ufshcd_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 		lrbp->cmd = NULL;
 		clear_bit_unlock(tag, &hba->lrb_in_use);
 		ufshcd_release_all(hba);
-		ufshcd_vops_pm_qos_req_end(hba, cmd->request, true);
 		goto out;
 	}
 
