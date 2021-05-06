1a52cb717cf5388916e33fa3fbad7578589ac41c
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-05-06 19:05:49 +0800

    build: compile with GCC
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-CLANG_DIR=~/clang/bin/clang
-
 if [ ! -f out/.config ]; then
 	make O=out rmx2170_defconfig
 else
 	make O=out oldconfig
 fi
 
-export KBUILD_COMPILER_STRING=$($CLANG_DIR --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-
-make CC="ccache $CLANG_DIR" CLANG_TRIPLE=aarch64-linux-gnu- \
-     CROSS_COMPILE=~/gcc-7.4.1/bin/aarch64-linux-gnu- \
+make CROSS_COMPILE=~/gcc-7.4.1/bin/aarch64-linux-gnu- \
      TARGET_PRODUCT=atoll -j4 Image.gz dtbs
 
 ATOLL="out/arch/arm64/boot/dts/qcom/atoll.dtb"
