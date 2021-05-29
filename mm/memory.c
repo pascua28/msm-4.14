4f45642b8a10c709c814196fa97ab0f48166ac69
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-05-29 18:56:51 +0800

    mm: cleanup oppo changes
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3042,7 +3042,7 @@ int do_swap_page(struct vm_fault *vmf)
 	struct mem_cgroup *memcg;
 	swp_entry_t entry;
 	struct swap_info_struct *si;
-	int swp_cnt;
+	bool skip_swapcache = false;
 	pte_t pte;
 	int locked;
 	int exclusive = 0;
@@ -3084,15 +3084,24 @@ int do_swap_page(struct vm_fault *vmf)
 
 
 	delayacct_set_flag(DELAYACCT_PF_SWAPIN);
+
+	/*
+	 * lookup_swap_cache below can fail and before the SWP_SYNCHRONOUS_IO
+	 * check is made, another process can populate the swapcache, delete
+	 * the swap entry and decrement the swap count. So decide on taking
+	 * the SWP_SYNCHRONOUS_IO path before the lookup. In the event of the
+	 * race described, the victim process will find a swap_count > 1
+	 * and can then take the readahead path instead of SWP_SYNCHRONOUS_IO.
+	 */
 	si = swp_swap_info(entry);
-	swp_cnt = __swap_count(si, entry);
+	if (si->flags & SWP_SYNCHRONOUS_IO && __swap_count(si, entry) == 1)
+		skip_swapcache = true;
+
 	page = lookup_swap_cache(entry, vma, vmf->address);
 	swapcache = page;
 
 	if (!page) {
-
-		if (si->flags & SWP_SYNCHRONOUS_IO && swp_cnt == 1) {
-			/* skip swapcache */
+		if (skip_swapcache) {
 			page = alloc_page_vma(GFP_HIGHUSER_MOVABLE, vma,
 							vmf->address);
 			if (page) {
