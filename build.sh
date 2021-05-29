7072549317d166490c3b3ab1b610cf265d8a7c3b
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-05-29 16:34:53 +0800

    add AK3
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build.sh
+++ build.sh
@@ -12,12 +12,24 @@ export KBUILD_COMPILER_STRING=$($CLANG_DIR --version | head -n 1 | perl -pe 's/\
 
 make CC=$CLANG_DIR CLANG_TRIPLE=aarch64-linux-gnu- \
      CROSS_COMPILE=~/gcc-7.4.1/bin/aarch64-linux-gnu- \
-     TARGET_PRODUCT=atoll -j4 Image.gz dtbs
+     TARGET_PRODUCT=atoll -j8 Image.gz-dtb
 
 ATOLL="out/arch/arm64/boot/dts/qcom/atoll.dtb"
 ATOLL_AB="out/arch/arm64/boot/dts/qcom/atoll-ab.dtb"
 DTB_OUT="out/arch/arm64/boot/dts/qcom/206B1.dtb"
+IMAGE="out/arch/arm64/boot/Image.gz"
 
 if [[ -f "$ATOLL" && -f "$ATOLL_AB" ]]; then
      cat "$ATOLL" "$ATOLL_AB" > $DTB_OUT
+fi
+
+
+if [[ -f "$IMAGE" ]]; then
+	rm AnyKernel3/Image.gz > /dev/null 2>&1
+	rm AnyKernel3/*.zip > /dev/null 2>&1
+	rm AnyKernel3/dtb > /dev/null 2>&1
+	cp $IMAGE AnyKernel3/Image.gz
+	cp $DTB_OUT AnyKernel3/dtb
+	cd AnyKernel3
+	zip -r release.zip .
 fi
